c1cf9332c5ce93c03e33d3b1973832bf25def932
ShiHao <shihao316558512@hotmail.com>
2016-02-27 17:41:42 +0900

    修该index.html

---
+++ Scripts/default.js
@@ -0,0 +1,70 @@
+﻿$(function () {
+    pageInitModule.setWidth();
+    pageInitModule.setSidebar();
+    pageInitModule.setScrollToTop();
+    $(window).resize(function () {
+        pageInitModule.setWidth();
+    })
+    $(window).scroll(function () {
+        pageInitModule.setScrollToTop();
+    });
+})
+
+/*
+* init page when page load
+*/
+var pageInitModule = (function (mod) {
+    mod.setWidth = function () {
+        if ($(window).width() < 768) {
+            $(".sidebar").css({ left: -220 });
+            $(".all").css({ marginLeft: 0 });
+        } else {
+            $(".sidebar").animate({ left: 0 });
+            $(".all").animate({ marginLeft: 220 });
+        }
+    };
+    mod.setScrollToTop = function () {
+        var top = $(window).scrollTop();
+        if (top < 60) {
+            $('#goTop').hide();
+        } else {
+            $('#goTop').show();
+        }
+    };
+    mod.setSidebar = function () {
+        $('[data-target="sidebar"]').click(function () {
+            var asideleft = $(".sidebar").offset().left;
+            if (asideleft == 0) {
+                $(".sidebar").animate({ left: -220 });
+                $(".all").animate({ marginLeft: 0 });
+            }
+            else {
+                $(".sidebar").animate({ left: 0 });
+                $(".all").animate({ marginLeft: 220 });
+            }
+        });
+        $(".has-sub>a").click(function () {
+            $(this).parent().siblings().find(".sub-menu").slideUp();
+            $(this).parent().find(".sub-menu").slideToggle();
+        })
+        var _strcurrenturl = window.location.href;
+        $(".nav li").each(function () {
+            var isActive = false;
+            var currentTarget = $(this);
+            $(".breadcrumb>li a").each(function (index) {
+                if (currentTarget.find("a").attr("href") == $(this).attr("href")) {
+                    isActive = true;
+                    return false;
+                }
+            })
+            if (_strcurrenturl.indexOf($(this).find(">a").attr("href")) > -1 || isActive) {
+                $(this).addClass("active");
+                if ($(this).parent().attr("class") == "sub-menu") {
+                    $(this).parent().slideDown();
+                    $(this).parents(".has-sub").addClass("active");
+                }
+            }
+        })
+    }
+    return mod;
+})(window.pageInitModule || {});
---
+++ Scripts/jquery.extend.js
@@ -0,0 +1,85 @@
+﻿/*******
+made by shihao
+2015.04.03
+*******/
+//$.tools = {
+//    getQueryString: function (name) {
+//        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+//        var r = window.location.search.substr(1).match(reg);
+//        if (r != null) return unescape(r[2]); return null;
+//    },
+//    validateInt: function (value) {
+//        var reg = /^\d+$/;
+//        if (value.constructor === String) {
+//            var re = value.match(reg);
+//            if (re != null) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        }
+//        return false;
+//    },
+//    validateFloat: function (value) {
+//        var reg = /^[0-9]+([.]{1}[0-9]{1,2})?$/;
+//        if (value.constructor === String) {
+//            var re = value.match(reg);
+//            if (re != null) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        }
+//        return false;
+//    }
+//}
+$.tools = (function () {
+    /*
+    * getQueryString
+    */
+    var getQueryString = function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]); return null;
+    };
+    /*
+    * validateInt
+    */
+    var validateInt = function (value) {
+        var reg = /^\d+$/;
+        if (value.constructor === String) {
+            var re = value.match(reg);
+            if (re != null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    };
+    /*
+    * validateFloat
+    */
+    var validateFloat = function (value) {
+        var reg = /^[0-9]+([.]{1}[0-9]{1,2})?$/;
+        if (value.constructor === String) {
+            var re = value.match(reg);
+            if (re != null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    };
+
+
+    return {
+        getQueryString: getQueryString
+    , validateInt: validateInt
+    , validateFloat: validateFloat
+    };
+
+})();
+
+
